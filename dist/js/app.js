c8d468d6b3d3c8b02c9b78a9a7255bd3fa3aaac7
葛方帅 <gefangshuai@163.com>
2016-01-24 19:51:32 +0800

    coding

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,10 @@
 var setMoreHouseHeight = function () {
-    var $w = $(window).width();
+    var $w = $(window).width() > 970 ? $(window).width() : 970;
     var padW = 340 / 1920 * $w;
     var mTop = 207 / 1920 * $w;
     var mButton = 75 / 1920 * $w;
     var fButton = 286 / 1920 * $w;
+    var nextImgTop = 505 / 1920 * $w;
 
     $('div.more-house').css({
         paddingLeft: padW,
@@ -13,6 +14,8 @@ var setMoreHouseHeight = function () {
     $('div.jianzi').css({marginTop: mTop, marginBottom: mButton});
 
     $('.thumbnail').css({marginBottom: fButton});
+
+    $('.next-img,.previous-img').css({top: nextImgTop});
 };
 
 $(window).resize(function () {
